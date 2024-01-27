85c8b49352d633c8754fdf78c8194ff7ee3e7590
dkzanz1 <paul.zolik@sky.com>
2024-01-27 11:14:20 +0000

    creating hamburger icon with react-bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,37 @@
 import React from "react";
-import { Container, Row, Col, Nav, NavDropdown, Button } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Button, Navbar} from "react-bootstrap";
 import Logo from "./logo";
-//created using react-strap
+
 function NavBar() {
     return<>
-    {/* styled with bootstrap */}
-    <Container>
-        <Row className="justify-content-between">
-            <Col>
+    {/* styled with react-bootstrap;:- It allows you to use Bootstrap's components directly within your React application without the need for jQuery or Bootstrap's own JavaScript*/}
+    <Navbar expand="lg" bg="light" variant="light">
+        <Container>
+            <Navbar.Brand>
                 <Logo />
-            </Col>
-            <Col>
-                <Nav className="justify-content-end">
-                  
-                <Nav.Item>                             
-                        <Nav.Link  href="/">Contact</Nav.Link>
-                </Nav.Item> 
-                <NavDropdown title="About us" id="basic-nav-dropdown">
-                          
-                    <NavDropdown.Item href="/">Action</NavDropdown.Item> 
-                    <NavDropdown.Item href="/">Next Action</NavDropdown.Item>
-                    <NavDropdown.Item href="/">Another Action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="/">Separate Action</NavDropdown.Item>
-                </NavDropdown>
-                       <Nav.Item>                           
+            </Navbar.Brand>    
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Item>                             
+                            <Nav.Link  href="/">Contact</Nav.Link>
+                        </Nav.Item> 
+                                <NavDropdown title="About us" id="basic-nav-dropdown">
+                                        
+                                    <NavDropdown.Item href="/">Action</NavDropdown.Item> 
+                                    <NavDropdown.Item href="/">Next Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="/">Another Action</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="/">Separate Action</NavDropdown.Item>
+                                </NavDropdown>
+                            <Nav.Item>                           
                         <Nav.Link href="/">Projects</Nav.Link>
                         </Nav.Item> 
-                      
-                    
                         <Button className="make-contact-btn">Get in Touch</Button>
-    
-                     </Nav>
-            </Col>
-        </Row>
-    </Container>
-   
+                    </Nav>
+                    </Navbar.Collapse>
+        </Container>
+    </Navbar>
     
     </> 
     }
